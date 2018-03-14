c716597e6cc2d9598d8ed43fb171bf7e6df97c5d
rockwolf <thereisanewway@gmail.com>
2018-03-14 22:54:08 +0100

    Added plot-income-vs-expenses skeleton. Add real code later.

--- ledgerplot-cl.lisp
+++ ledgerplot-cl.lisp
@@ -21,14 +21,29 @@
   (format t "Options:~%")
   (format t "~4tpath/to/ledger.dat: main ledger.dat file to create charts for~%"))
 
-(defun process-ledger-file (a-ledger-file-str)
-  "Process ledger file, for creating charts."
+(defun plot-income-vs-expenses ()
+  "Plot income-vs-expenses."
+  (progn
+    (prepare-plot-income-vs-expenses)
+    (generate-plot-income-vs-expenses)))
+
+(defun prepare-plot-income-vs-expenses ()
+  "Prepare the data to use in the income-vs-expenses plot."
+  (format t "Preparing data... TBD.~%"))
+
+(defun generate-plot-income-vs-expenses ()
+  "Generate the plot income-vs-expenses."
   ; TODO: the below is just a dummy example.
   (defparameter *x-list* (loop for i from (- pi) to pi by 0.1 collect i))
   (clgp:splot (lambda (x y) (+ (sin x) (cos y)))
-              *x-list* ; x
-              *x-list* ; y
-              :view-point '(20 45) :z-scale 1.5))
+               *x-list* ; x
+               *x-list* ; y
+               :view-point '(20 45) :z-scale 1.5))
+
+(defun process-ledger-file (a-ledger-file-str)
+  "Process ledger file, for creating charts."
+  ; TODO: Add other charts.
+  (plot-income-vs-expenses))
 
 (defun main ()
   "Main code processing.
