d67400713a7fad6b532d4ab6f3b0cbd7cf503935
Andy Nagels <Andy_Nagels@peopleware.be>
2018-04-06 16:23:30 +0200

    Best version so far. Center X-axis. No left axis though. Do I need it?

--- plots/income-vs-expenses.lisp
+++ plots/income-vs-expenses.lisp
@@ -25,20 +25,35 @@
   (eazy-gnuplot::with-plots
     (*standard-output* :debug nil)
     (eazy-gnuplot::gp-setup
+      ; begin output setup
       :output output
       :terminal '(pngcairo enhanced font "Liberation Mono,10")
       ;:terminal '(pngcairo enhanced font "Liberation Mono,10" size 800 #\, 600)
-      :title "Income vs expenses per year"
       :style '(data histogram)
       :style '(histogram clustered gap 1)
       :style '(fill transparent solid noborder)
-      :xtics '("nomirror scale 0 center")
-      ;:ytics '("add ('' 0) scale 0")
-      :ytics '("add ('' 0) axis nomirror scale 0")
-      :grid '(ytics)
-      :border 1
+      ;:border '(1 front linetype -1 linewidth 1.0)
+      :border '(0)
       :key '(bmargin center horizontal reverse noenhanced autotitles columnhead nobox)
+      ; end output setup
+      ; begin titles/labels
+      :title "Income vs expenses per year"
+      :xlabel "Year"
+      :ylabel "Value (EUR)"
+      ; end titles/labels
+      ; begin grid/axis
+      :xtics '("border in scale 0,0 nomirror rotate by -45 offset character 0, 0, 0 autojustify")
+      :ytics '("border in scale 0,0 nomirror rotate by -45 offset character 0, 0, 0 autojustify")
+      :ztics '("border in scale 0,0 nomirror rotate by -45 offset character 0, 0, 0 autojustify")
+      :cbtics '("border in scale 0,0 nomirror rotate by -45 offset character 0, 0, 0 autojustify")
+      :rtics '("axis in scale 0,0 nomirror rotate by -45 offset character 0, 0, 0 autojustify")
+      :grid '(nopolar xtics nomxtics ytics nomytics noztics nomztics nox2tics nomx2tics noy2tics nomy2tics nocbtics nomcbtics)
+      :xzeroaxis '(linetype -1 linewidth 1.0)
+      ;:yzeroaxis '(linetype 0 linewidth 1.0)
+      ;:zzeroaxis '(linetype 0 linewidth 1.0)
+      ; end grid/axis
     )
+    ; Income data
     (eazy-gnuplot::plot
       ;#P"/var/tmp/ledgeroutput1.tmp"
       #P"ledgeroutput1.tmp"
@@ -51,22 +66,20 @@
       :with '(labels left rotate by 45 offset "-4,0.5")
       :textcolor
       :linestyle 0
-      :notitle
-    )
+      :notitle)
+    ; Expenses data
     (eazy-gnuplot::plot
       ;#P"/var/tmp/ledgeroutput2.tmp"  
       #P"ledgeroutput2.tmp"
       :using '(2)
       :title "Expenses"
-      :linecolor '(rgb "light-salmon")
-     )
+      :linecolor '(rgb "light-salmon"))
     (eazy-gnuplot::plot
       "''"
       :using '(0 2 2)
       :with '(labels left rotate by 45 offset "0,0.5")
       :textcolor
       :linestyle 0
-      :notitle
-    )
+      :notitle)
     )
   output)
